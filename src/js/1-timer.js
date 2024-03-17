966252b85b8605ca7be414a70ce8f0ee334392f1
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-03-17 21:27:42 +0100

    changed styles

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,3 +2,17 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+const input = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("[data-start]");
+const timer = document.querySelector("timer");
+
+
